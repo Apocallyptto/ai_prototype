b0b2ed57778987b61189892435c9a73fc617c4f6
Nicolas Dojčan <NDojcan@gmail.com>
2025-08-30 19:50:14 +0200

    Update db.py

--- lib/db.py
+++ lib/db.py
@@ -4,5 +4,10 @@ import sqlalchemy as sa
 @st.cache_resource
 def get_engine():
     s = st.secrets["db"]
-    url = f"postgresql+psycopg2://{s['user']}:{s['password']}@{s['host']}:{s['port']}/{s['dbname']}"
+    url = (
+        f"postgresql+psycopg2://{s['user']}:{s['password']}"
+        f"@{s['host']}:{s['port']}/{s['dbname']}?sslmode=require"
+    )
+    # If you ever get a “channel binding required” error, append:
+    # url += "&channel_binding=require"
     return sa.create_engine(url, pool_pre_ping=True)
