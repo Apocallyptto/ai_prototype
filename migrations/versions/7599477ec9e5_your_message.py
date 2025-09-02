53b8c1b0c3fffca860319a6509d33ee248dd6cff
Nicolas Dojčan <NDojcan@gmail.com>
2025-09-03 00:10:04 +0200

    Create 7599477ec9e5_your_message.py

---
+++ migrations/versions/7599477ec9e5_your_message.py
@@ -0,0 +1,22 @@
+"""your message
+
+Revision ID: 7599477ec9e5
+Revises: 01e34ac8170f
+Create Date: 2025-09-03 00:09:45.916998
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7599477ec9e5'
+down_revision = '01e34ac8170f'
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
