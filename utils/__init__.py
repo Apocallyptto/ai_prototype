10b539f7770cae589bc67a7aa40aaa4dc424e9f0
Nicolas Dojčan <NDojcan@gmail.com>
2025-11-25 21:07:24 +0100

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,17 +1,7 @@
-# utils/__init__.py
-"""
-Central place to re-export helper functions used across the project.
-"""
+from lib.db import get_engine
+from tools.atr import compute_atr
 
-from lib.db import make_engine          # lib/db.py
-from tools.atr import compute_atr       # tools/atr.py
-
-
-def get_engine():
-    """
-    Thin wrapper around lib.db.make_engine() so old code can use utils.get_engine().
-    """
-    return make_engine()
-
-
-__all__ = ["get_engine", "compute_atr"]
+__all__ = [
+    "get_engine",
+    "compute_atr",
+]
